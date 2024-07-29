0c8bdd383bb3da4e3bf92901a0f3611970b3152b
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-29 15:41:42 +0300

    Hero Anımation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import ArrowIcon from "../assets/icons/arrow-w.svg";
 import cursorImage from "../assets/images/cursor.png";
 import messageImage from "../assets/images/message.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -28,20 +30,36 @@ const Hero = () => {
             <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center  inline-flex">
               One Task <br /> at a Time
             </h1>
-            <Image
-              src={cursorImage}
-              alt=""
-              height="200"
-              width="200"
-              className="absolute right-[476px] top-[108px] hidden sm:inline"
-            />
-            <Image
-              src={messageImage}
-              alt=""
-              height="200"
-              width="200"
+            <motion.div
+              className="absolute right-[476px] top-[108px] hidden sm:inline
+            
+            "
+              drag
+              dragSnapToOrigin
+            >
+              <Image
+                src={cursorImage}
+                alt=""
+                height="200"
+                width="200"
+                className="max-w-none"
+                draggable="false"
+              />
+            </motion.div>
+            <motion.div
               className="absolute top-[56px] left-[498px] hidden sm:inline"
-            />
+              drag
+              dragSnapToOrigin
+            >
+              <Image
+                src={messageImage}
+                alt=""
+                height="200"
+                width="200"
+                className="max-w-none"
+                draggable="false"
+              />
+            </motion.div>
           </div>
         </div>
 
