cd8c3d74bd4e19acdb13a711b14f376e1aef3931
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-29 15:33:52 +0300

    Footer Section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,34 @@
+import InstaIcon from "../assets/icons/insta.svg";
+import XSocial from "../assets/icons/x-social.svg";
+import TicTokIcon from "../assets/icons/tiktok.svg";
+import YoutubeIcon from "../assets/icons/youtube.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="container">
+        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+          <div className="text-center">
+            © 2024 Ahmet Gülmez, Inc. All rights reserved
+          </div>
+          <ul className="flex justify-center gap-2.5">
+            <li>
+              <XSocial />
+            </li>
+            <li>
+              <InstaIcon />
+            </li>
+            <li>
+              <TicTokIcon />
+            </li>
+            <li>
+              <YoutubeIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
