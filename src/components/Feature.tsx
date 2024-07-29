8f44f92d563c9076c9eeac210add0066a955cb15
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-29 16:51:02 +0300

    Features Animation

---
+++ src/components/Feature.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+const Feature = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  const offsetX = useMotionValue(-100);
+  const offsetY = useMotionValue(-100);
+  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px,black,transparent)`;
+  const border = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      //e.x
+      //e.y
+      if (!border.current) return;
+      const borderRect = border.current?.getBoundingClientRect();
+      offsetX.set(e.x - borderRect.x);
+      offsetY.set(e.x - borderRect.y);
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative">
+        <motion.div
+          className="absolute inset-0 border-2 border-purple-400 rounded-xl"
+          style={{
+            WebkitMaskImage: maskImage,
+            maskImage,
+          }}
+          ref={border}
+        ></motion.div>
+        <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+          <EcosystemIcon />
+        </div>
+        <h3 className="mt-6 font-bold ">{title}</h3>
+        <p className="mt-2 text-white/70">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
