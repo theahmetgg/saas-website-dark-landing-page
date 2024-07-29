a7322821f3c526ba4b6bb299691df956f331f49a
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-29 16:54:58 +0300

    Features Animation bug fix

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,7 +1,7 @@
-"use client";
 import React, { useEffect, useRef } from "react";
 import EcosystemIcon from "../assets/icons/ecosystem.svg";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+
 const Feature = ({
   title,
   description,
@@ -11,22 +11,23 @@ const Feature = ({
 }) => {
   const offsetX = useMotionValue(-100);
   const offsetY = useMotionValue(-100);
-  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px,black,transparent)`;
+  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black, transparent)`;
   const border = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const updateMousePosition = (e: MouseEvent) => {
-      //e.x
-      //e.y
       if (!border.current) return;
-      const borderRect = border.current?.getBoundingClientRect();
+      const borderRect = border.current.getBoundingClientRect();
       offsetX.set(e.x - borderRect.x);
-      offsetY.set(e.x - borderRect.y);
+      offsetY.set(e.y - borderRect.y);
     };
+
     window.addEventListener("mousemove", updateMousePosition);
+
     return () => {
       window.removeEventListener("mousemove", updateMousePosition);
     };
-  }, []);
+  }, [offsetX, offsetY]);
 
   return (
     <div>
@@ -35,7 +36,7 @@ const Feature = ({
           className="absolute inset-0 border-2 border-purple-400 rounded-xl"
           style={{
             WebkitMaskImage: maskImage,
-            maskImage,
+           WebkitMaskBoxImage: maskImage,
           }}
           ref={border}
         ></motion.div>
