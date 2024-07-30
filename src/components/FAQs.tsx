fc755ad313ff6974c2ac3f33f262f2b42a2d023d
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-30 15:51:01 +0300

    FAQs Animation

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import PlusIcon from "../assets/icons/plus.svg";
 import MinusIcon from "../assets/icons/minus.svg";
 import clsx from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 const items = [
   {
@@ -51,9 +52,26 @@ const AccordionItem = ({
           <PlusIcon className="cursor-pointer" />
         )}
       </div>
-      <div className={clsx("mt-4", { hidden: !isOpen, "": isOpen === true })}>
-        {answer}
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+           
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px",
+            }}
+            exit={{ opacity: 0, height: 0, marginTop: 0 }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
